2e54bb73af4aeadf9ca115ade81dd16ff1e50fcf
Junior <jesusdev29@gmail.com>
2021-03-16 14:05:51 -0400

    diseño del chat

--- chat-app/src/components/SidebarChatItems.js
+++ chat-app/src/components/SidebarChatItems.js
@@ -1,4 +1,4 @@
-import React, { useContext }  from 'react'
+import React, { useContext } from 'react'
 import { ChatContext } from '../chat/ChatContext'
 import { fecthContoken } from '../helpers/fecth';
 import { scrollToBottom } from '../helpers/scrollMensaje';
@@ -7,10 +7,9 @@ import { types } from '../types/types'
 
 export const SidebarChatItems = ({ user }) => {
 
-    const { chatState, dispatch } = useContext( ChatContext);
-    const { chatActivo } = chatState
+    const { dispatch } = useContext(ChatContext);
 
-    const onClickUserChat = async()=>{
+    const onClickUserChat = async () => {
 
         dispatch({
             type: types.selectUserChat,
@@ -19,7 +18,7 @@ export const SidebarChatItems = ({ user }) => {
 
         //solicitar los mensajes de el chat seleccionado
         const mensajes = await fecthContoken(`mensaje/${user.uid}`);
-     
+
         dispatch({
             type: types.cragarMensajes,
             payload: mensajes.mensajeDe.reverse()
@@ -30,26 +29,30 @@ export const SidebarChatItems = ({ user }) => {
 
     return (
 
-        <div 
-            className={`chat_list ${(user.uid === chatActivo) && "active_chat" }`}
-            onClick={ onClickUserChat }
+        <div className="card-chat" onClick={onClickUserChat}>
 
-        >
-            <div className="chat_people">
-                <div className="chat_img">
-                    <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil" />
-                </div>
-                <div className="chat_ib">
-                <h5>{user.name}</h5>
-
-                    {
-                        (user.online)
-                            ? <span className="text-success">online</span>
-                            :<span className="text-danger">offline</span>
-                    } 
-                            
+            <div className="card-avatar">
+                <img src="https://ptetutorials.com/images/user-profile.png" alt="no fount" className="design-img"></img>
+                <div className="border-chat">
+                    <div className={` ${(user.online) ? "activo" : "inactivo"}`}>
+                    </div>
                 </div>
             </div>
+
+            <div className="card-contenido">
+                <div className="card-text">
+                    <h3 className="name-chat">{user.name}</h3>
+                    <div className="row-chat">
+                        <p className="">You: Thanks, sounds good. What about doing a webinar, too?</p>
+                        <span className="separador">.</span>
+                        <time> 8hr </time>
+                    </div>
+                </div>
+                <div className="card-indicador">
+                    <img src="https://ptetutorials.com/images/user-profile.png" alt="no fount" className="design-img-icon"></img>
+                </div>
+            </div>
+
         </div>
 
     )
